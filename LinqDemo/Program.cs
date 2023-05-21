7f62bf7ffab80e9f2de0473fbd82139c82c7987a
kzmuda <kh@wp.pl>
2023-05-21 14:20:38 +0200

    .

--- LinqDemo/Program.cs
+++ LinqDemo/Program.cs
@@ -8,7 +8,42 @@ namespace LinqDemo
     {
         static void Main(string[] args)
         {
-            
+            var students = Student.GetStudents();
+
+
+            // var result = from s in students
+            //     where s.Age >= 18
+            //         select s;
+
+            var result =
+                students
+                    .Where(x => x.Age >= 18)
+                    .Where(x => x.StudentID > 1)
+                    ;
+
+            foreach (var student in result)
+            {
+                Console.Out.WriteLine(student);
+            }
+
+            students.Add(new Student()
+            {
+                StudentID = 15, StudentName = "aaa", Age = 20
+            });
+
+            foreach (var student in result)
+            {
+                Console.Out.WriteLine(student);
+            }
+
+            var s = students
+                .Where(x => x.Age >= 18)
+                .Select(x => new Employee() { Name = x.StudentName})
+                .ToList();
+
+            int x = 10;
+            x.IsGreaterThan(15);
+
         }
     }
 }
